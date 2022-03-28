9b81a7438629c86e8835159ad5a1ebaec588337c
Matěj Šela <matejsela@gmail.com>
2022-03-28 10:23:03 +0200

    added stump

--- Assets/Assets/Scripts/Trees, Rocks/Tree.cs
+++ Assets/Assets/Scripts/Trees, Rocks/Tree.cs
@@ -9,12 +9,14 @@ public class Tree : MonoBehaviour{
     private GameObject log;
     private GameObject[] sticks;
     private GameObject stick;
+    private GameObject stump;
     private Vector3 position;
     void Start(){
         position = gameObject.transform.position;
         position.y += 2.1f;
         sticks = new GameObject[Random.Range(3,5)];
         log = Resources.Load<GameObject>("Prefabs/log");
+        stump = Resources.Load<GameObject>("Prefabs/Stump");
         for (int i = 0; i < sticks.Length; i++) sticks[i] = Resources.Load<GameObject>("Prefabs/stick");
         stick = Resources.Load<GameObject>("Prefabs/stick");
     }
@@ -25,19 +27,27 @@ public class Tree : MonoBehaviour{
         }
     }
 
-    private void OnCollisionEnter(Collision other){
-        if (other.gameObject.CompareTag("Axe")){
-            hp -= 1;
-            SpawnStick(stick);
-        }
+    private void OnCollisionEnter(Collision other) {
+        if (!other.gameObject.CompareTag("Axe")) return;
+        hp -= 1;
+        SpawnStick(stick);
     }
-    private void Destroyed(){
-        var logs = Instantiate(log, position, Quaternion.Euler(90f, 0f, 0f));
+    private void Destroyed() {
+        var logPosition = new Vector3(position.x, position.y + 0.2f, position.z);
+        var stumpPosition = new Vector3(position.x, position.y - 2.7f, position.z);
+        var logs = Instantiate(log, logPosition, Quaternion.Euler(90f, 0f, 0f));
+        var stumps = Instantiate(stump, stumpPosition, Quaternion.identity);
+        
         foreach (var sticki in sticks){
             SpawnStick(sticki);
         }
-        logs.transform.parent = gameObject.transform.parent;
-        Destroy(gameObject);
+
+        var o = gameObject;
+        var parent = o.transform.parent;
+        
+        logs.transform.parent = parent;
+        stumps.transform.parent = parent;
+        Destroy(o);
     }
 
     private void SpawnStick(GameObject sticki){
